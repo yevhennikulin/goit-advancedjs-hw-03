0f6a3e6a76a24296064e07b1beb90016f8699973
Yevhen Nikulin <nikulinevg@gmail.com>
2026-06-24 08:02:56 +0300

    Complete HW 3

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_KEY = '47407182-ea00b0f5cb08deb97ea3953d6';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export async function getImagesByQuery(query) {
+  const params = {
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  };
+
+  const response = await axios.get(BASE_URL, { params });
+  return response.data;
+}
